b1112436f53576324cfd9578f5c9c6e8a58a1e1b
Pranavi Reddy Madadi <pranavi.madadi@gmail.com>
2016-09-07 08:59:03 -0400

    Lab 10 Movie

---
+++ co/grandcircus/studentlist/Movie.java
@@ -0,0 +1,32 @@
+package co.grandcircus.studentlist;
+
+public class Movie {
+
+	private String name;
+	private String category;
+	
+	
+	 public Movie(String name, String category) {
+		// TODO Auto-generated constructor stub
+		this.name= name;
+		this.category= category;
+	}
+
+	public String getName(){
+		return name;
+	}
+	
+	public String setName(){
+		return name;
+	}
+	
+	public String getCategory(){
+		return category;
+	}
+	
+	public String setCategory(){
+		return category;
+	}
+	
+	
+}
